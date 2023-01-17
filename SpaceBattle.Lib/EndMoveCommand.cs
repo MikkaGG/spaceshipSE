34f3e3f3e9e76ff8d9faec8175cd60149090440f
aleksandr <n03aa13@gmail.com>
2023-01-17 18:57:36 +0600

    Added change

--- SpaceBattle.Lib/EndMoveCommand.cs
+++ SpaceBattle.Lib/EndMoveCommand.cs
@@ -8,6 +8,7 @@ public class EndMoveCommand : ICommand {
     public void Execute() {
         ICommand EndCommand = IoC.Resolve<ICommand>("Command.EmptyCommand");
         IoC.Resolve<ICommand>("Game.DeleteProperty", obj.UObject, "velocity").Execute();
+        IoC.Resolve<ICommand>("Game.MyObject.GetUObj", obj).Execute();
         IoC.Resolve<ICommand>("Game.InjectCommand", obj.Queue, EndCommand).Execute();
     }
 }
