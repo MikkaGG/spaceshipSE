3644d34a5e025120e3de35da7c5ddf65fcd13465
Egor <shikin03@inbox.ru>
2023-01-19 14:41:45 +0600

    Added files lab3

---
+++ SpaceBattle.Lib/CheckCollision.cs
@@ -0,0 +1,24 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+
+namespace BattleSpace.Lib {
+    public class CollisionCheck : ICommand
+    {
+        private readonly IUObject uobj_1, uobj_2;
+        public CollisionCheck(IUObject _uobj_1, IUObject _uobj_2)
+        {
+            this.uobj_1 = _uobj_1;
+            this.uobj_2 = _uobj_2;
+        }
+        
+        public void Execute()
+        {   
+            var list = IoC.Resolve<IEnumerable<int>>("Collision.GetDeltas", uobj_1, uobj_2);
+
+            if (IoC.Resolve<bool>("Collision.CheckWithTree", list)) 
+            {
+                throw new Exception("Collision");
+            }
+        }
+    }
+}
