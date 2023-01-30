e66188b9b251ffcd59aec7bf62b02bdf7be70f68
aleksandr <n03aa13@gmail.com>
2023-01-30 12:51:07 +0600

    added DeleteProperty

--- SpaceBattle.Lib/DeleteProperty.cs
+++ SpaceBattle.Lib/DeleteProperty.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 
 namespace BattleSpace.Lib.Test;
 
-public class DeleteProperty : Hwdtech.ICommand {
+public class DeleteProperty : ICommand {
     IUObject obj; 
     IList<string> properties;
     IPropertyDelete propertyDelete;
-    public DeleteProperty(IPropertyDelete propertyDelete, IUObject obj,  IList<string> properties){
+    public DeleteProperty(IPropertyDelete propertyDelete, IUObject obj,  IList<string> properties) {
         this.obj = obj;
         this.properties = properties;
         this.propertyDelete = propertyDelete;
     }
     public void Execute() {
         for (int i = 0; i < properties.Count; i++) {
-            propertyDelete.DeleteProperty(obj ,properties[i]);
+            propertyDelete.DeleteProperty(obj, properties[i]);
         }
     }
 }
