a75ac92d11584b327412b49840e9829e8877fc07
aleksandr <n03aa13@gmail.com>
2023-01-30 12:55:38 +0600

    EndMoveCommandTest

--- SpaceBattle.Lib.Test/EndMoveCommandTest.cs
+++ SpaceBattle.Lib.Test/EndMoveCommandTest.cs
@@ -8,45 +8,68 @@ public class EndMoveCommandTests {
     public EndMoveCommandTests() {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-        var CommandMock = new Mock<ICommand>();
-        var regStrategy = new Mock<IStrategy>();
-        var GetObj = new Mock<IStrategy>();
-        regStrategy.Setup(m => m.ExecuteStrategy(It.IsAny<object[]>())).Returns(CommandMock.Object);
-        GetObj.Setup(m => m.ExecuteStrategy(It.IsAny<object[]>())).Returns(CommandMock.Object);
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Command.EmptyCommand", (object[] args) => regStrategy.Object.ExecuteStrategy(args)).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.DeleteProperty", (object[] args) => regStrategy.Object.ExecuteStrategy(args)).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.InjectCommand", (object[] args) => regStrategy.Object.ExecuteStrategy(args)).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.MyObject.GetUObj", (object[] args) => regStrategy.Object.ExecuteStrategy(args)).Execute();
+
+        var deleteProperty = new Mock<IPropertyDelete>();
+        var someObj = new Mock<IUObject>();
+        deleteProperty.Setup(p => p.DeleteProperty(someObj.Object, It.IsAny<string>()));
+        var deletePropertyStrategy = new DeletePropertyStrategy(deleteProperty.Object);
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.DeleteObjectPropertyCommand", (object[] args) => deletePropertyStrategy.ExecuteStrategy(args)).Execute();
+        
+        var injectStrategy = new InjectCommandStrategy();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.EmptyCommand", (object[] args) => injectStrategy.ExecuteStrategy(args)).Execute();
+    
+        var queueStrategy = new Mock<IStrategy>();
+        queueStrategy.Setup(p => p.ExecuteStrategy(It.IsAny<object[]>()));
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.Push", (object[] args) => queueStrategy.Object.ExecuteStrategy(args)).Execute();
     }
-    // [Fact]
-    // public void EndMoveCommandPos() {
-    //     var moveendable = new Mock<IMoveCommandEndable>();
+
+    [Fact]
+    public void EndMoveCommandPos() {
+        var moveendable = new Mock<IMoveCommandEndable>();
      
-    //     moveendable.SetupGet(m => m.UObject).Returns(new Mock<IUObject>().Object);
-    //     moveendable.SetupGet(m => m.Queue).Returns(new Mock<Queue<ICommand>>().Object);
-    //     ICommand EndMoveCommand = new EndMoveCommand(moveendable.Object);
-    //     EndMoveCommand.Execute();
-    //     moveendable.VerifyAll();
-    // }
+        moveendable.SetupGet<IUObject>(m => m.UObject).Returns(new Mock<IUObject>().Object);
+        moveendable.SetupGet<ICommand>(m => m.Cmd).Returns(new Mock<ICommand>().Object);
+        moveendable.SetupGet<IList<string>>(m => m.Properties).Returns(new Mock<IList<string>>().Object);
 
-    // [Fact]
-    // public void EndMoveCommandUnreadableObject() {
-    //     var moveendable = new Mock<IMoveCommandEndable>();
+        var endMoveCommand = new EndMoveCommand(moveendable.Object);
+        endMoveCommand.Execute();
+        moveendable.VerifyAll();
+    }
 
-    //     moveendable.SetupGet(m => m.UObject).Throws(new Exception());
-    //     moveendable.SetupGet(m => m.Queue).Returns(new Mock<Queue<ICommand>>().Object);
-    //     ICommand endMoveCommand = new EndMoveCommand(moveendable.Object);
-    //     Assert.Throws<Exception>(() => endMoveCommand.Execute());
-    // }
+    [Fact]
+    public void UnableGetObjectTest() {
+        var moveendable = new Mock<IMoveCommandEndable>();
+     
+        moveendable.SetupGet<IUObject>(m => m.UObject).Throws<Exception>();
+        moveendable.SetupGet<ICommand>(m => m.Cmd).Returns(new Mock<ICommand>().Object);
+        moveendable.SetupGet<IList<string>>(m => m.Properties).Returns(new Mock<IList<string>>().Object);
 
-    // [Fact]
-    // public void EndMoveCommandUnreadableQueue() {
-    //     var moveendable = new Mock<IMoveCommandEndable>();
+        var endMoveCommand = new EndMoveCommand(moveendable.Object);
+        Assert.Throws<Exception>(() => endMoveCommand.Execute());
+    }
+    
+    [Fact]
+    public void UnableGetCommandTest() {
+        var moveendable = new Mock<IMoveCommandEndable>();
+     
+        moveendable.SetupGet<IUObject>(m => m.UObject).Returns(new Mock<IUObject>().Object);
+        moveendable.SetupGet<ICommand>(m => m.Cmd).Throws<Exception>();
+        moveendable.SetupGet<IList<string>>(m => m.Properties).Returns(new Mock<IList<string>>().Object);
 
-    //     moveendable.SetupGet(m => m.UObject).Returns(new Mock<IUObject>().Object);
-    //     moveendable.SetupGet(m => m.Queue).Throws(new Exception());
-    //     ICommand endMoveCommand = new EndMoveCommand(moveendable.Object);
-    //     Assert.Throws<Exception>(() => endMoveCommand.Execute());
-    // }    
+        var endMoveCommand = new EndMoveCommand(moveendable.Object);
+        Assert.Throws<Exception>(() => endMoveCommand.Execute());
+    }
+    
+    [Fact]
+    public void UnableGetPropertiesTest() {
+        var moveendable = new Mock<IMoveCommandEndable>();
+     
+        moveendable.SetupGet<IUObject>(m => m.UObject).Returns(new Mock<IUObject>().Object);
+        moveendable.SetupGet<ICommand>(m => m.Cmd).Returns(new Mock<ICommand>().Object);
+        moveendable.SetupGet<IList<string>>(m => m.Properties).Throws<Exception>();
+
+        var endMoveCommand = new EndMoveCommand(moveendable.Object);
+        Assert.Throws<Exception>(() => endMoveCommand.Execute());
+    }
 }
 
