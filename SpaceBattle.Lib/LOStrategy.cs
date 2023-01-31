b50a1753f90f5e59e9758d9209a352ee2d5297fa
aleksandr <n03aa13@gmail.com>
2023-01-31 16:59:50 +0600

    added LOStartegy

--- SpaceBattle.Lib/LOStrategy.cs
+++ SpaceBattle.Lib/LOStrategy.cs
@@ -4,9 +4,9 @@ public class LOstrategy : IStrategy {
     public object ExecuteStrategy(params object[] args){
         string name = (string)args[0];
         IUObject obj = (IUObject)args[1];
-        var MacroCommand = IoC.Resolve<ICommand>("BuildMacroCommandStrat", name, obj);
-        ICommand RepeatCommand = IoC.Resolve<ICommand>("Game.RepeadCommand", MacroCommand);
-        ICommand InjectCommand = IoC.Resolve<ICommand>("Game.InjectCommand", RepeatCommand);
+        var Command = IoC.Resolve<ICommand>("Game.Command.CreateCommand", name, obj);
+        ICommand RepeatCommand = IoC.Resolve<ICommand>("Game.Command.RepeatCommand", Command);
+        ICommand InjectCommand = IoC.Resolve<ICommand>("Game.Command.InjectCommand", RepeatCommand);
         return InjectCommand;
     }
 }
